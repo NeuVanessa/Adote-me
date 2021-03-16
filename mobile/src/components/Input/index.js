91c1d8b2301b47b840d435b2b67a8285fe3f8a34
Vanessa Lopes <vanessashirleneestrela@gmail.com>
2021-03-15 23:19:55 -0300

    Apagando Código não utilizado

--- mobile/src/components/Input/index.js
+++ mobile/src/components/Input/index.js
@@ -1,12 +1,5 @@
 import React, { Component } from "react";
 import {
-  View,
-  Text,
-  TouchableOpacity,
-  TextInput,
-  StyleSheet,
-} from "react-native";
-import {
   SimpleInput,
   Input,
   ButtonLogin,
