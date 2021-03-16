3cd68e38a501471269c4f963524dd869f8dede5f
Vanessa Lopes <vanessashirleneestrela@gmail.com>
2021-03-15 23:41:44 -0300

    Atualizano o Login para receber rota de Home

--- mobile/src/components/Input/index.js
+++ mobile/src/components/Input/index.js
@@ -7,7 +7,6 @@ import {
   StyleSheet,
 } from "react-native";
 import {
-
   SimpleInput,
   Input,
   ButtonLogin,
@@ -17,50 +16,47 @@ import {
   TextFooter,
 } from "./styles";
 
-
 //import Icon from "react-native-vector-icons/FontAwesome";
-
-import Icon from "react-native-vector-icons/FontAwesome";
-
-class Inputs extends Component {
-  render() {
-    return (
-      <SimpleInput>
-        <Input
-          underlineColorAndroid="transparent"
-          placeholder="Username"
-          placeholderTextColor="#723D2B"
-          autoCapitalize="none"
-          onChangeText={this.handleEmail}
-        />
-
-        <Input
-          underlineColorAndroid="transparent"
-          placeholder="Password"
-          placeholderTextColor="#723D2B"
-          autoCapitalize="none"
-          onChangeText={this.handlePassword}
-        />
-
-        <ButtonLogin
-          onPress={() => this.login(this.state.email, this.state.password)}
-        >
-          <ButtonText color="#fff">Logar</ButtonText>
-        </ButtonLogin>
-        <ButtonGmail
-          onPress={() => this.login(this.state.email, this.state.password)}
-        >
-          <ButtonText color="#fff">Continue with Google</ButtonText>
-        </ButtonGmail>
-        <ButonRegister
-          onPress={() => this.login(this.state.email, this.state.password)}
-        >
-          <ButtonText color="#fff">Cadastrar</ButtonText>
-        </ButonRegister>
-
-        <TextFooter color="#fff">Copyright © 2021 Equipe Adote-me.</TextFooter>
-      </SimpleInput>
-    );
-  }
+function click() {
+  alert("cliclou");
 }
-export default Inputs;
+
+export default function Login() {
+  //função para teste do botão, criei essa função simples de clique para ver se o botão realmente estava funcionando.
+
+  return (
+    <SimpleInput>
+      <Input
+        underlineColorAndroid="transparent"
+        placeholder="Username"
+        placeholderTextColor="#723D2B"
+        autoCapitalize="none"
+        onChangeText={this.handleEmail}
+      />
+
+      <Input
+        underlineColorAndroid="transparent"
+        placeholder="Password"
+        placeholderTextColor="#723D2B"
+        autoCapitalize="none"
+        onChangeText={this.handlePassword}
+      />
+
+      <ButtonLogin onPress={click}>
+        <ButtonText color="#fff">Logar</ButtonText>
+      </ButtonLogin>
+      <ButtonGmail
+        onPress={() => this.login(this.state.email, this.state.password)}
+      >
+        <ButtonText color="#fff">Continue with Google</ButtonText>
+      </ButtonGmail>
+      <ButonRegister
+        onPress={() => this.login(this.state.email, this.state.password)}
+      >
+        <ButtonText color="#fff">Cadastrar</ButtonText>
+      </ButonRegister>
+
+      <TextFooter color="#fff">Copyright © 2021 Equipe Adote-me.</TextFooter>
+    </SimpleInput>
+  );
+}
