6df77072fdd2ce6f9aa1e148f6620ba0404570ef
Thiago <thiagoF89.tf@gmail.com>
2021-03-26 16:52:40 -0300

    :tada: adicionado tela de termos de uso

--- mobile/src/pages/Home/index.js
+++ mobile/src/pages/Home/index.js
@@ -4,31 +4,35 @@ import Header from '../../components/Header';
 import {Text}from 'react-native';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
+import Terms_of_Use from '../../pages/Terms_of_Use'
+
 import CreateRecent from "./CreateRecent";
+
 export default function Home() {
     
     return (
-        <Container>
+        // <Container>
 
-            <Header search />
+        //     <Header search />
 
-            <ContainerBody>
-                <Text style={{left:16,fontSize:15,top:10,color:"#fff"}}>Adicionados Recentimente</Text>
-                <ContainerMain>
-                    <CreateRecent />
-                </ContainerMain>
+        //     <ContainerBody>
+        //         <Text style={{left:16,fontSize:15,top:10,color:"#fff"}}>Adicionados Recentimente</Text>
+        //         <ContainerMain>
+        //             <CreateRecent />
+        //         </ContainerMain>
 
-            <ConitanerRecent>
-                <Text style={{color:"#fff"}}>
-                ESSE CONTAINER É PARA TRABALHAR COM as OUTRAS POSTAGENS 
-                </Text>
+        //     <ConitanerRecent>
+        //         <Text style={{color:"#fff"}}>
+        //         ESSE CONTAINER É PARA TRABALHAR COM as OUTRAS POSTAGENS 
+        //         </Text>
 
-            </ConitanerRecent>
+        //     </ConitanerRecent>
 
-            <ContainerButton>
-                <Text> ESTE CONTAINER É PARA TRABALHAR COM O BOTÃO CRIAR ANUNCIO! Xd</Text>
-            </ContainerButton>
-            </ContainerBody>
-        </Container>
+        //     <ContainerButton>
+        //         <Text> ESTE CONTAINER É PARA TRABALHAR COM O BOTÃO CRIAR ANUNCIO! Xd</Text>
+        //     </ContainerButton>
+        //     </ContainerBody>
+        // </Container>
+        <Terms_of_Use />
     );
 }
---
+++ mobile/src/pages/Terms_of_Use/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+`
+
+export const Header = styled.View`
+  margin-top: 7%;
+  width: 100%;
+  height: 70px;
+  background-color: #723D2B;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 20px;
+`
+
+export const Content = styled.View`
+  width: 100%;
+  max-height: 80%;
+  padding: 20px 15px;
+  background-color: #ddd;
+  border-radius: 8px;
+`
