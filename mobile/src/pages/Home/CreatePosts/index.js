c5ebe58af7b94d8938a53458980a2c8efa11eea5
Vanessa Lopes <vanessashirlenee_estrela@hotmail.com>
2021-03-26 17:41:08 -0300

    🎉 Arrumando o CreatePosts

--- mobile/src/pages/Home/CreatePosts/index.js
+++ mobile/src/pages/Home/CreatePosts/index.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { ScrollView, Text, SafeAreaView, Image } from "react-native";
 import { Container, Card, CardText } from "./styles";
 
 export default function CreateRecent() {
-  const array = [
+  const [array,setArray] = useState([
     {
       id: 1,
       category: "LABRADOR",
@@ -70,7 +70,129 @@ export default function CreateRecent() {
       uri:
         "https://www.gazetadigital.com.br/uploads/webdisco/2017/02/17/jpg/original/350205d21d6f464f1c5a4fb8e47807a2.jpg",
     },
-  ];
+    {
+      id: 6,
+      category: "LABRADOR",
+      idade: "1 Mês",
+      raça: "Pitbull",
+      name: "jose",
+      description: "Atendido por José gosta de fica no sofá.",
+      datePublication: "13 Marco 16:54, Paralela",
+      uri:
+        "https://www.gazetadigital.com.br/uploads/webdisco/2017/02/17/jpg/original/350205d21d6f464f1c5a4fb8e47807a2.jpg",
+    },
+    {
+      id: 6,
+      category: "LABRADOR",
+      idade: "1 Mês",
+      raça: "Pitbull",
+      name: "jose",
+      description: "Atendido por José gosta de fica no sofá.",
+      datePublication: "13 Marco 16:54, Paralela",
+      uri:
+        "https://www.gazetadigital.com.br/uploads/webdisco/2017/02/17/jpg/original/350205d21d6f464f1c5a4fb8e47807a2.jpg",
+    },
+    {
+      id: 6,
+      category: "LABRADOR",
+      idade: "1 Mês",
+      raça: "Pitbull",
+      name: "jose",
+      description: "Atendido por José gosta de fica no sofá.",
+      datePublication: "13 Marco 16:54, Paralela",
+      uri:
+        "https://www.gazetadigital.com.br/uploads/webdisco/2017/02/17/jpg/original/350205d21d6f464f1c5a4fb8e47807a2.jpg",
+    },
+    {
+      id: 6,
+      category: "LABRADOR",
+      idade: "1 Mês",
+      raça: "Pitbull",
+      name: "jose",
+      description: "Atendido por José gosta de fica no sofá.",
+      datePublication: "13 Marco 16:54, Paralela",
+      uri:
+        "https://www.gazetadigital.com.br/uploads/webdisco/2017/02/17/jpg/original/350205d21d6f464f1c5a4fb8e47807a2.jpg",
+    },
+    {
+      id: 6,
+      category: "LABRADOR",
+      idade: "1 Mês",
+      raça: "Pitbull",
+      name: "jose",
+      description: "Atendido por José gosta de fica no sofá.",
+      datePublication: "13 Marco 16:54, Paralela",
+      uri:
+        "https://www.gazetadigital.com.br/uploads/webdisco/2017/02/17/jpg/original/350205d21d6f464f1c5a4fb8e47807a2.jpg",
+    },
+    {
+      id: 6,
+      category: "LABRADOR",
+      idade: "1 Mês",
+      raça: "Pitbull",
+      name: "jose",
+      description: "Atendido por José gosta de fica no sofá.",
+      datePublication: "13 Marco 16:54, Paralela",
+      uri:
+        "https://www.gazetadigital.com.br/uploads/webdisco/2017/02/17/jpg/original/350205d21d6f464f1c5a4fb8e47807a2.jpg",
+    },
+    {
+      id: 6,
+      category: "LABRADOR",
+      idade: "1 Mês",
+      raça: "Pitbull",
+      name: "jose",
+      description: "Atendido por José gosta de fica no sofá.",
+      datePublication: "13 Marco 16:54, Paralela",
+      uri:
+        "https://www.gazetadigital.com.br/uploads/webdisco/2017/02/17/jpg/original/350205d21d6f464f1c5a4fb8e47807a2.jpg",
+    },
+    {
+      id: 6,
+      category: "LABRADOR",
+      idade: "1 Mês",
+      raça: "Pitbull",
+      name: "jose",
+      description: "Atendido por José gosta de fica no sofá.",
+      datePublication: "13 Marco 16:54, Paralela",
+      uri:
+        "https://www.gazetadigital.com.br/uploads/webdisco/2017/02/17/jpg/original/350205d21d6f464f1c5a4fb8e47807a2.jpg",
+    },
+    {
+      id: 6,
+      category: "LABRADOR",
+      idade: "1 Mês",
+      raça: "Pitbull",
+      name: "jose",
+      description: "Atendido por José gosta de fica no sofá.",
+      datePublication: "13 Marco 16:54, Paralela",
+      uri:
+        "https://www.gazetadigital.com.br/uploads/webdisco/2017/02/17/jpg/original/350205d21d6f464f1c5a4fb8e47807a2.jpg",
+    },
+    {
+      id: 6,
+      category: "LABRADOR",
+      idade: "1 Mês",
+      raça: "Pitbull",
+      name: "jose",
+      description: "Atendido por José gosta de fica no sofá.",
+      datePublication: "13 Marco 16:54, Paralela",
+      uri:
+        "https://www.gazetadigital.com.br/uploads/webdisco/2017/02/17/jpg/original/350205d21d6f464f1c5a4fb8e47807a2.jpg",
+    },
+    {
+      id: 6,
+      category: "LABRADOR",
+      idade: "1 Mês",
+      raça: "Pitbull",
+      name: "jose",
+      description: "Atendido por José gosta de fica no sofá.",
+      datePublication: "13 Marco 16:54, Paralela",
+      uri:
+        "https://www.gazetadigital.com.br/uploads/webdisco/2017/02/17/jpg/original/350205d21d6f464f1c5a4fb8e47807a2.jpg",
+    },
+    
+  ]);
   return (
     <Container>
       <Text
@@ -92,9 +214,9 @@ export default function CreateRecent() {
                 <Image
                   style={{
                     width: 120,
-                    height: 150,
+                    height: 140,
                     top: 0,
-                    left: 0,
+                    left:10,
                     position: "absolute",
                   }}
                   source={{
--- mobile/src/pages/Home/CreatePosts/styles.js
+++ mobile/src/pages/Home/CreatePosts/styles.js
@@ -1,28 +1,28 @@
 import styled from "styled-components/native";
 import colors from "../../../utils/colors";
 export const Container = styled.View`
-  top: 20px;
+  /* top: 20px;
   flex: 1;
   flex-direction: row;
-  background-color: ${colors.secudary};
-  flex-grow:40px;
+  background-color: ${colors.primary};
+  flex-grow:40px; */
 
+  flex: 1;
+  background-color:${colors.secudary};
+  padding-top: 40;
 `;
 
 export const Card = styled.View`
   flex: 1;
-  top: 45px;
-  margin-left: 8px;
-  width: 539px;
-  margin:5px;
-  height: 150px;
-  background-color:${colors.white};
-
+  padding-top: -0px;
+  background-color: ${colors.secudary};
 `;
 
 export const CardText = styled.View`
-  flex: 1;
-  top:10px;
-  margin-left: 150px;
-  
+  margin-top: 2px;
+  width:400px;
+  padding: 25px;
+  font-size: 10px;
+  left: 122px;
+  background-color:${colors.white};
 `;
