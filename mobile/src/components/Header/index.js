61963b6fe680038826ce4bc2d483761a3bf2ea78
Vanessa Lopes <vanessashirlenee_estrela@hotmail.com>
2021-03-26 02:11:41 -0300

    🎉 Botão Adicionar Anuncio Home

--- mobile/src/components/Header/index.js
+++ mobile/src/components/Header/index.js
@@ -65,22 +65,15 @@ export default function Header({ title, search }) {
   return (
     <Container
     sbehavior={Platform.OS === "ios" ? "padding" : ""} enabled
-      // retirado porque ocupa a tela toda, ai limitei a altura no styles.js
-      // style={{
-      //   width: widthPercentageToDP("100%"),
-      //   height: heightPercentageToDP("150%"),
-      // }} 
     >
       <ContainerOut>
         <Icon
-          // style={{ position: 'absolute', left: 10 }}
+          
           name="reorder-three-outline"
           color="white"
           size={40}
         />
       </ContainerOut>
-       {/* CODIGO DO PESQUISAR ALGO DESABILITADO|COMENTADO ESSE CÓDIGO SERÁ MEXIDO EM ALGUMA ISSUE ABERTA|  */}
-
        {title && (
          <TitleText>{title}</TitleText>
        )}
@@ -103,5 +96,3 @@ export default function Header({ title, search }) {
     </Container>
   );
 }
-
-//style={{ left: 100, top: 20, position: "absolute" }}
